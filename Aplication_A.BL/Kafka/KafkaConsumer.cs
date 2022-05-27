795fc3b69d82d20010a965e6f2e818d5152d18ab
4ervenkov749s <dimitar_87@abv.bg>
2022-05-27 22:28:21 +0300

    latest

--- Aplication_A.BL/Kafka/KafkaConsumer.cs
+++ Aplication_A.BL/Kafka/KafkaConsumer.cs
@@ -1,4 +1,5 @@
-﻿using Aplication_A.Models;
+﻿using Aplication_A.BL.Services;
+using Aplication_A.Models;
 using Confluent.Kafka;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -8,55 +9,56 @@ using System.Threading.Tasks;
 namespace Aplication_A.BL.Kafka
 {
     public class KafkaConsumer : IHostedService
+    {
+        private static IConsumer<byte[], Person> _consumer;
+
+        public KafkaConsumer()
         {
-            private static IConsumer<int, Person> _consumer;
-
-            public KafkaConsumer()
+            var config = new ConsumerConfig
             {
-                var config = new ConsumerConfig
-                {
-                    BootstrapServers = "localhost",
-                    AutoCommitIntervalMs = 5000,
-                    FetchWaitMaxMs = 50,
-                    GroupId = Guid.NewGuid().ToString(),
-                    AutoOffsetReset = AutoOffsetReset.Earliest,
-                    EnableAutoCommit = true,
-                    ClientId = "2",
-                };
+                BootstrapServers = "localhost",
+                AutoCommitIntervalMs = 5000,
+                FetchWaitMaxMs = 50,
+                GroupId = Guid.NewGuid().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = true,
+                ClientId = "2",
+            };
 
-                _consumer = new ConsumerBuilder<int, Person>(config)
-                    .SetValueDeserializer(new MsgPackDeserializer<Person>())
-                    .Build();
-            }
-
-            public Task StartAsync(CancellationToken cancellationToken)
-            {
-
-                _consumer.Subscribe("test");
-                Task.Factory.StartNew(() =>
-                {
-                    while (!cancellationToken.IsCancellationRequested)
-                    {
-                        try
-                        {
-                            var cr = _consumer.Consume(cancellationToken);
-                            Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}' . ");
-                        }
-                        catch (ConsumeException e)
-                        {
-                            Console.WriteLine($"Error occured: {e.Error.Reason}");
-                        }
-                    }
-                }, cancellationToken);
-
-                return Task.CompletedTask;
-            }
-
-            public Task StopAsync(CancellationToken cancellationToken)
-            {
-                return Task.CompletedTask;
-            }
+            _consumer = new ConsumerBuilder<byte[], Person>(config)
+                .SetValueDeserializer(new MsgPackDeserializer<Person>())
+                .Build();
         }
 
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            _consumer.Subscribe("person");
+            Task.Factory.StartNew(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var cr = _consumer.Consume(cancellationToken);
+                        PersonList.person.Add(cr.Message.Value);
+                        Console.WriteLine($"Consumed message '{cr.Message.Value.Name}' at: '{cr.TopicPartitionOffset}' . ");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
+
+}
 
---
+++ Aplication_A.BL/Services/PersonList.cs
@@ -0,0 +1,10 @@
+﻿using Aplication_A.Models;
+using System.Collections.Generic;
+
+namespace Aplication_A.BL.Services
+{
+    public static class PersonList
+    {
+        public static List<Person> person = new List<Person>();
+    }
+}
